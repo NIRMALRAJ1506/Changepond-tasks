e56a44c2387f33a244dce5c576ae0cd391eaf4a2
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2024-06-18 22:53:49 +0530

    task new commit

---
+++ imgtoggle/src/Shared/Constant/staticData.js
@@ -0,0 +1,15 @@
+import ajith from '../Images/ajith.webp';
+import vijay from '../Images/vijay.webp';
+import cena from '../Images/cena.jpg';
+import rdj from '../Images/rdj.jpg';
+import cr7 from '../Images/cr7.jpg';
+import messi from '../Images/messi.jpg';
+import eminem from '../Images/eminem.jpg';
+import dhoni from '../Images/dhoni.jpg';
+import vk from '../Images/vk.webp';
+import rock from '../Images/rock.jpg';
+
+const staticData={
+    ajith,vijay,cena,rdj,cr7,messi,eminem,dhoni,vk,rock
+}
+export default staticData;
